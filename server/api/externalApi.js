83118e6b2383e18f7df09b536ff9fb12b22142f5
Guli <gulikholmatova@gmail.com>
2019-01-24 12:54:16 -0500

    implemented google facial recognition and installed dependencies for this

--- server/api/externalApi.js
+++ server/api/externalApi.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const vision = require('@google-cloud/vision')
 const GoogleAPIKey = './secrets/GoogleAPIKey.json'
-const image = '/guliSad.jpeg'
+const path = require('path')
+app.use(express.static(path.join(__dirname, '..', 'public')))
+const imageUrl = './public/guliSad.jpeg'
+const fs = require('fs')
 
 // Imports the Google Cloud client library &
 // Creates a client
@@ -10,20 +13,12 @@ const client = new vision.ImageAnnotatorClient({
   keyFilename: GoogleAPIKey
 })
 
-client
-  .labelDetection(
-    'http://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/e4R-RRY/person-smiling-and-happy_sqtmh7xr_thumbnail-full03.png' // happy
-  )
-  .then(results => {
-    const labels = results[0].labelAnnotations
-    console.log('results[0].labelAnnotations', labels)
+async function detectFaces(inputFile) {
+  // Make a call to the Vision API to detect the faces
+  const request = {image: {source: {filename: inputFile}}}
+  const response = await client.faceDetection(request)
+  const facialData = response[0].faceAnnotations[0]
+  console.log('results[0].faceAnnotations[0]', facialData)
+}
 
-    const faces = results[0].facelAnnotations
-    console.log('results[0].faceAnnotations', faces)
-
-    console.log('Labels:')
-    labels.forEach(label => console.log(label))
-  })
-  .catch(err => {
-    console.error('ERROR:', err)
-  })
+detectFaces(imageUrl)
