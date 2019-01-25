8fa4426f2cbe9c9b9e64278062ecc07c699b9732
Claire Gilligan <feminaprovita@gmail.com>
2019-01-25 17:12:07 -0500

    style: removed unnecessary boilerplate components

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,60 +1,16 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
 import SpotifyContainer from './spotify-container'
 import RecommendationsButton from './RecommendationsButton'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = () => (
   <div>
-    <h1>MOODIFY</h1>
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <SpotifyContainer />
-          <RecommendationsButton />
-        </div>
-      )}
+      <h1>MOODIFY</h1>
+      <SpotifyContainer />
+      <RecommendationsButton />
     </nav>
     <hr />
   </div>
 )
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+export default Navbar
